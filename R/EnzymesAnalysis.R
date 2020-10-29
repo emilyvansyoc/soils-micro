fb7f9d0b9933e94e71253a763a9ce63b3ee8385b
Emily Bean <epb5360@psu.edu>
2020-10-29 11:26:05 -0400

    permanovas and corrs

--- R/EnzymesAnalysis.R
+++ R/EnzymesAnalysis.R
@@ -2,9 +2,14 @@
 
 require(tidyverse)
 require(vegan)
-require(ggordiplots)
+if(!require(ggordiplots)) {
+  install.packages("remotes")
+  remotes::install_github("jfq3/ggordiplots")
+  require(ggordiplots)
+}
 require(emmeans)
 source("https://raw.githubusercontent.com/EmilyB17/grazing_soil_microbes/master/R/CustomFunctions/FUNCTION_percentDifferences.R")
+source("./R/RColorBrewer.R")
 
 # read cn for C:DOC and N:DON ratios
 cn <- read.table("./data/2018CN.txt", sep = "\t", header = TRUE) %>% 
@@ -328,3 +333,223 @@ ggplot(data = enzna, aes(x = lnPHOS, y = lnBG)) +
   geom_text(label = "r2 = 0.004", x = 2, y = 6.3) +
   ggtitle("BG vs PHOS")
 
+## ---- EEA PERMANOVAs ----
+
+# get bacterial and fungal community data
+require(phyloseq)
+load("./data/phyloseq16S_ITS.RData")
+
+# get enzyme data and calculate relative abundnace with vegan's decostand
+enzr <- enz %>% 
+  select(AG, BG, BX, CBH, LAP, NAG, PER1, PEROX, PHENOX, PHOS)
+
+enzd <- decostand(enzr, method = "total", MARGIN = 2, na.rm = TRUE)
+
+## get bacterial and fungal Phyla for NMDS metadata (only bulk samples)
+tgf <- tax_glom(subset_samples(psITS18, psITS18@sam_data$sample_type != "rhizospheric"), 
+                taxrank = rank_names(psITS18)[2], NArm = TRUE)
+taxa_names(tgf) <- tgf@tax_table@.Data[,2]
+# make into dataframe
+df <- as.data.frame(otu_table(tgf))
+# get relative abundance
+dc <- decostand(df, method = "total", MARGIN = 2)
+colnames(dc) <- gsub("p__", "fungi_", colnames(dc))
+# same for 16S
+tgs <- tax_glom(subset_samples(ps16S18, ps16S18@sam_data$sample_type != "rhizospheric"),
+                taxrank = rank_names(ps16S18)[2], NArm = TRUE)
+taxa_names(tgs) <- tgs@tax_table@.Data[,2]
+# make into dataframe
+dfs <- as.data.frame(otu_table(tgs))
+dcs <- decostand(dfs, method = "total", MARGIN = 2)
+colnames(dcs) <- paste0("bac_", colnames(dcs))
+dcs <- dcs %>% rownames_to_column(var = "name")
+
+# ITS has 71 samples while 16S has 72 - make the missing sample of ITS into "NAs"
+which(!row.names(dcs) %in% row.names(dc))
+row.names(dcs[35,]) # PRE_8_B
+dc["PRE_8_B",] <- 0
+dc <- dc[order(row.names(dc)),]
+dc <- dc %>% rownames_to_column(var = "name")
+
+# combine with other sample data
+sd <- sd16S18 %>% 
+  filter(sample_type == "bulk") %>% 
+  select(-c(AG, BG, BX, CBH, LAP, NAG, PER1, PEROX, PHENOX, PHOS, biomass_kg_plot)) %>% 
+  rownames_to_column(var = "name") %>% 
+  left_join(dc, by = "name") %>% 
+  left_join(dcs, by = "name") %>% 
+  column_to_rownames(var = "name")
+
+# PERMANOVA testing
+dis <- vegdist(enzd)
+adonis(dis ~ Treatment * GrazeTime, data = sd, permutations = 999) # GrazeTime is significant
+
+# ordinate
+ord <- metaMDS(enzd, distance = "bray", k = 2, trymax = 1000, previousBest = TRUE)
+stressplot(ord, dis) # OK with 2 dimensions
+eft <- envfit(ord, env = sd, perm = 1000, na.rm = TRUE)
+
+## PLOT NMDS
+## 1.  get NMDS coordinates from ordination object
+coords <- as.data.frame(scores(ord, display = "sites")) %>% 
+  # and add grouping variable
+  mutate(Treatment = sd$Treatment,
+         Time = sd$GrazeTime)
+
+## 2.  get significant envfit objects to plot as arrows
+spp <- as.data.frame(scores(eft, display = "vectors")) 
+spp$species <- rownames(spp)
+# subset to only p < 0.05 vectors to de-clutter 
+sigs <- data.frame(eft$vectors$arrows) %>% 
+  filter(eft$vectors$pvals < 0.05,)
+sigspecies <- rownames(sigs)
+# subset
+sigspecies <- spp %>% filter(species %in% sigspecies)
+
+## 3. Pull ellipse information using gg_ordiplot package
+# save gg_ordiplot object to get ellipse values
+plot <-  gg_ordiplot(ord, groups = sd$GrazeTime, label = FALSE, plot = FALSE)
+# get ellipse coordinates
+df_ell <- plot$df_ellipse
+# get label coordinates for ellipse centers
+NMDS.mean <- plot$df_mean.ord
+# pull NMDS coordinates
+ord.data <- plot$df_ord 
+
+## Plot by TIME - WITH LABELS
+## create in ggplot2
+ggplot(data = coords, aes(x = NMDS1, y = NMDS2)) + # label axises automatically
+  # ELLIPSES
+  geom_path(data = df_ell, aes(x = x, y = y, color = Group), show.legend = FALSE) +
+  # ORDINATION POINTS
+  geom_point(data = coords, aes(x = NMDS1, y = NMDS2, color = Time), size = 1) +
+  # GROUP NAMES AT ELLIPSE CENTER 
+  annotate("text",x = NMDS.mean$x, y = NMDS.mean$y,label=NMDS.mean$Group, size = 5) +
+  # ENVFIT ARROWS
+  geom_segment(data = sigspecies,
+               aes(x = 0, xend = NMDS1, y = 0, yend = NMDS2),
+               arrow = arrow(length = unit(0.25, "cm")), colour = "grey") +
+  # ARROW TEXT
+  geom_text(data = sigspecies, aes(x = NMDS1, y = NMDS2, label = species),
+            size = 3) +
+  # SCALE CORRECTLY
+  coord_fixed() +
+  # THEME
+  theme_bw() +
+  # CHANGE LEGEND TITLE
+  labs(color = "Sampling Time") +
+  # add constant colors for each Time
+  scale_color_manual(values = timecols)
+
+## ---- Edaphic PERMANOVA ----
+
+# Euclidean distance PERMANOVA with soils variables
+
+# normalize to relative abundance
+sdf <- cn %>% 
+  select(-c(NO3_mgkgdrysoil, mineralN_mgkgdrysoil, Plot, Block, Treatment, GrazeTime))
+sdf <- decostand(sdf, method = "total", MARGIN = 2)
+# make distance matrix
+dis <- vegdist(sdf, method = "euclidean")
+
+# get sample data
+sd <- sd16S18 %>% 
+  filter(sample_type == "bulk") %>% 
+  select(-c(biomass_kg_plot, grav_mois, DON_mgkgdrysoil, NPOC_mgkgdrysoil,
+            mineralN_mgkgdrysoil, NH4_mgkgdrysoil, NO3_mgkgdrysoil, sample_type)) %>% 
+  rownames_to_column(var = "name") %>% 
+  left_join(dc, by = "name") %>% 
+  left_join(dcs, by = "name") %>% 
+  column_to_rownames(var = "name")
+
+# PERMANOVA
+adonis(dis ~ Treatment * GrazeTime, data = sd) # GrazeTime is significant
+
+# Ordinate
+ord <- metaMDS(sdf,  k = 2, trymax = 1000, previousBest = TRUE)
+stressplot(ord, dis)
+eft <- envfit(ord, env = sd, perm = 1000, na.rm  =TRUE)
+
+# Plot by GrazeTime
+## PLOT NMDS
+## 1.  get NMDS coordinates from ordination object
+coords <- as.data.frame(scores(ord, display = "sites")) %>% 
+  # and add grouping variable
+  mutate(Treatment = sd$Treatment,
+         Time = sd$GrazeTime)
+
+## 2.  get significant envfit objects to plot as arrows
+spp <- as.data.frame(scores(eft, display = "vectors")) 
+spp$species <- rownames(spp)
+# subset to only p < 0.05 vectors to de-clutter 
+sigs <- data.frame(eft$vectors$arrows) %>% 
+  filter(eft$vectors$pvals < 0.05,)
+sigspecies <- rownames(sigs)
+# subset
+sigspecies <- spp %>% filter(species %in% sigspecies)
+
+## 3. Pull ellipse information using gg_ordiplot package
+# save gg_ordiplot object to get ellipse values
+plot <-  gg_ordiplot(ord, groups = sd$GrazeTime, label = FALSE, plot = FALSE)
+# get ellipse coordinates
+df_ell <- plot$df_ellipse
+# get label coordinates for ellipse centers
+NMDS.mean <- plot$df_mean.ord
+# pull NMDS coordinates
+ord.data <- plot$df_ord 
+
+## Plot by TIME - WITH LABELS
+## create in ggplot2
+ggplot(data = coords, aes(x = NMDS1, y = NMDS2)) + # label axises automatically
+  # ELLIPSES
+  geom_path(data = df_ell, aes(x = x, y = y, color = Group), show.legend = FALSE) +
+  # ORDINATION POINTS
+  geom_point(data = coords, aes(x = NMDS1, y = NMDS2, color = Time), size = 1) +
+  # GROUP NAMES AT ELLIPSE CENTER 
+  annotate("text",x = NMDS.mean$x, y = NMDS.mean$y,label=NMDS.mean$Group, size = 5) +
+  # ENVFIT ARROWS
+  geom_segment(data = sigspecies,
+               aes(x = 0, xend = NMDS1, y = 0, yend = NMDS2),
+               arrow = arrow(length = unit(0.25, "cm")), colour = "grey") +
+  # ARROW TEXT
+  geom_text(data = sigspecies, aes(x = NMDS1, y = NMDS2, label = species),
+            size = 3) +
+  # SCALE CORRECTLY
+  coord_fixed() +
+  # THEME
+  theme_bw() +
+  # CHANGE LEGEND TITLE
+  labs(color = "Sampling Time") +
+  # add constant colors for each Time
+  scale_color_manual(values = timecols)
+
+### ---- Mantel correlations ----
+
+## EEA vs soil abiotic profiles
+enzdis <- vegdist(enzd, method = "bray")
+sdis <- vegdist(sdf, method = "euclidean")
+
+mantel(xdis = sdis, ydis = enzdis, method = "spearman", na.rm = TRUE) # not significant
+plot(sdis ~ enzdis)
+
+# bacterial vs soil aboitic
+bacdis <- distance(subset_samples(ps16S18T, sample_type == "bulk"), method = "bray")
+mantel(xdis = sdis, ydis = bacdis, method = "spearman") # not significant
+
+# bacterial vs EEA
+mantel(xdis = bacdis, ydis = enzdis, method = "spearman") # not significant
+
+# fungal vs soil abiotic
+# fungi is missing one sample ("PRE_8_B") 
+fundis <- distance(subset_samples(psITS18T, sample_type == "bulk"), method = "bray")
+mantel(xdis = vegdist(sdf[-35,], method = "euclidean"), ydis = fundis, method = "spearman") # not significant
+
+# fungal vs EEA
+mantel(xdis = vegdist(enzd[-35,], method = "bray"), ydis = fundis, method = "spearman") # not significant
+
+# bact vs fungal
+bacs <- subset_samples(ps16S18T, rownames(ps16S18T@sam_data) != "PRE_8_B")
+bacs1 <- subset_samples(bacs, bacs@sam_data$sample_type == "bulk")
+bacd <- as.data.frame(otu_table(bacs1))
+mantel(xdis = fundis, ydis = vegdist(bacd, method = "bray"), method = "spearman") ## significant
+plot(fundis ~ vegdist(bacd, method = "bray"))
